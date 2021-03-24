63b3a7f1fe7c84ba9025bc9732dea140900cbe7c
myhaikudream <myhaikudream@yahoo.com>
2021-03-23 19:08:01 -0700

    commit tree 8aa47154c858919ec1b98946ab106e1432ccc6cb

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+function wakeDog(dogName="Byron", dogBreed="poodle") {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+  } 
+
+  function leashDog(dogName="Byron", dogBreed="poodle") {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+  } 
+
+  function walkToPark(dogName="Byron", dogBreed="poodle") {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  } 
+
+  function throwFrisbee(dogName="Byron", dogBreed="poodle") {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  } 
+
+  function walkHome(dogName="Byron", dogBreed="poodle") {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  } 
+
+  function unleashDog(dogName="Byron", dogBreed="poodle") {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+  } 
+
+const routine = [wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog
+  ]
